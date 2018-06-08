ca4627df73ee31a5b3561810b8b63c72a74bac96
Amber Roberts <amberrobertsvt@gmail.com>
2018-06-08 12:23:36 -0400

    Matching function: cards do flip, compare, match

--- js/app.js
+++ js/app.js
@@ -4,28 +4,36 @@
  * Create a list that holds all of your cards
  */
 
- const allCards = ['fa-diamond', 'fa-diamond',
-             'fa-paper-plane-o', 'fa-paper-plane-o',
-             'fa-anchor', 'fa-anchor',
-             'fa-bolt', 'fa-bolt',
-             'fa-cube', 'fa-cube',
-             'fa-leaf', 'fa-leaf',
-             'fa-bicycle', 'fa-bicycle',
-             'fa-bomb', 'fa-bomb'
+const allCards = [
+  "fa-diamond",
+  "fa-diamond",
+  "fa-paper-plane-o",
+  "fa-paper-plane-o",
+  "fa-anchor",
+  "fa-anchor",
+  "fa-bolt",
+  "fa-bolt",
+  "fa-cube",
+  "fa-cube",
+  "fa-leaf",
+  "fa-leaf",
+  "fa-bicycle",
+  "fa-bicycle",
+  "fa-bomb",
+  "fa-bomb"
 ];
 
- const card = document.getElementsByClassName('card');
+const card = document.getElementsByClassName("card");
 
- let moveCount = 0;
- const moves = document.querySelector('.moves');
- moves.textContent = 0;
+let moveCount = 0;
+const moves = document.querySelector(".moves");
+moves.textContent = 0;
 
- function addMove() {
-   // increase moves
-   moveCount++;
-   moves.innerHTML = moveCount/2; // displays a pair of moves as 1 move
- }
-
+function addMove() {
+  // increase moves
+  moveCount++;
+  moves.innerHTML = moveCount / 2; // displays a pair of moves as 1 move
+}
 
 /*
  * Display the cards on the page
@@ -34,57 +42,62 @@
  *   - add each card's HTML to the page
  */
 
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
 function createCard(card) {
   return `<li class="card" data-card="${card}"> <i class="fa ${card}"> </i> </li>`;
 }
 
- const deck = document.querySelector('.deck');
+const deck = document.querySelector(".deck");
 
 // shuffle deck, clear moves, clear arrays used for matching, reset stars
- function startGame() {
-  let cardTemplate = shuffle(allCards).map(function(card){
+function startGame() {
+  let cardTemplate = shuffle(allCards).map(function(card) {
     return createCard(card);
   });
-  deck.innerHTML = cardTemplate.join('');
+  deck.innerHTML = cardTemplate.join("");
   moves.textContent = 0;
   let matchedCards = [];
   let flippedCards = [];
   let totalClicks = [];
-  console.log('cards here');
+  console.log("cards here");
 }
 
 startGame();
 
 // log total clicks for scoring with star rating
-let stars = document.querySelectorAll('.fa-star');
+let stars = document.querySelectorAll(".fa-star");
 let starTotal = 3; // To call for scoring in the modal
 
-function starCount(stars) { // Remove one star after 24 moves
-  if (totalClicks.length === 24) { // or if (moveCount === 24)
+function starCount(stars) {
+  // Remove one star after 24 moves
+  if (totalClicks.length === 24) {
+    // or if (moveCount === 24)
     stars[0].remove();
     starTotal--;
   }
-  if (totalClicks.length === 34) {  // Remove another star after 34 moves
+  if (totalClicks.length === 34) {
+    // Remove another star after 34 moves
     stars[1].remove();
     starTotal--;
   }
-  if (totalClicks.length === 44) {// Zero stars after 44 moves
+  if (totalClicks.length === 44) {
+    // Zero stars after 44 moves
     stars[2].remove();
     starTotal--;
   }
@@ -100,36 +113,52 @@ function starCount(stars) { // Remove one star after 24 moves
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
- let flippedCards = [];
- let matchedCards = [];
- let totalClicks = [];
+let flippedCards = [];
+let matchedCards = [];
+let totalClicks = [];
 
-// credit for assistance and event.target tutorial, as well as moral support, to Julian Johannesen in the FEND track.
-deck.addEventListener('click', flipIt, false); //  TODO:  this isn't working
+// Function to flip the cards and compare/ match them.  Credit for assistance and event.target tutorial, as well as moral support, to Julian Johannesen in the FEND track.
+deck.addEventListener("click", flipIt, false);
 
-  function flipIt(event) {
-    if (flippedCards.length < 2) { // TODO: only works the first time?
-      event.target.classList.add('show', 'open');
-      flippedCards.push(event.target);
-      totalClicks.push(event.target);
-      if (event.target.classList.contains('card')) // credit Matthew Cranford
-      addMove();
-      starCount(stars);
-      console.log('flipped it');
-    }
+function flipIt(event) {
+  if (
+    flippedCards.length < 2 &&
+    !event.target.classList.contains("open") &&
+    !event.target.classList.contains("show") &&
+    !event.target.classList.contains("match")
+  ) {
+    flippedCards.push(event.target);
+    totalClicks.push(event.target);
+    event.target.classList.add("show", "open");
+    addMove();
+    starCount(stars);
+}
+// Compare the two currently flipped cards to see if they match, if so keep flipped
+    if (flippedCards.length == 2 && flippedCards[0].dataset.card == flippedCards[1].dataset.card) {
+          flippedCards[0].classList.add('match');
+          flippedCards[0].classList.add('open');
+          flippedCards[0].classList.add('show');
+          matchedCards.push(flippedCards);
 
-      // If the two cards don't match
-if (flippedCards.length == 2) {
+          flippedCards[1].classList.add('match');
+          flippedCards[1].classList.add('open');
+          flippedCards[1].classList.add('show');
+
+          winning(); // Check to see if the game has been won
+    console.log("flipped it");
+  }
+
+  // If the two cards don't match, flip back over
+  if (flippedCards.length == 2) {
     setTimeout(function() {
       flippedCards.forEach(function(card) {
-      card.classList.remove('open', 'show');
-    });
-    flippedCards = [];
+        card.classList.remove("open", "show");
+      });
+      flippedCards = [];
       console.log("time is up");
     }, 1000);
+  }
 }
-}
-
 
 // timer reference: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_setinterval2
 
@@ -156,10 +185,16 @@ function stopTimer() { // Stop timer and clear seconds/minutes from timer displa
 
 function winning() {
   if (matchedCards.length === allCards.length) {
+    console.log("winning!");
     // end game and open final score message box
     // call stars and time for message box
+  } else {
+      console.log("Keep trying");
+    }
   }
-}
+
+
+// ***** Restart Button function *****
 
 let restart = document.querySelector(".restart");
 restart.addEventListener("click", function(newGame) {
